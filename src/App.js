6dc16b14c38c02f2cf8798cc0bc85e17131ba665
Sanath Shetty <sanathshetty0405@gmail.com>
2024-11-13 12:23:04 +0530

    Updated app.js with new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import AdminDashboard from './AdminDashboard';  // Import Admin Dashboard
+import PlayerDashboard from './PlayerDashboard'; // Import Player Dashboard
+import Register from './Register'; // Import Register component
 import './App.css';
+import PlayerManagement from './modules/PlayerManagement';
+import ScheduleManagement from './modules/ScheduleManagement';
+import TeamManagement from './modules/TeamManagement';
+import ReportsManagement from './modules/Reports';
+import PlayerStats from './modules2/PlayerStats';
+import ScheduleView from './modules2/ScheduleView';
+import FitnessTracker from './modules2/FitnessTracker';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/admindashboard" element={<AdminDashboard />} />
+                    <Route path="/playerdashboard" element={<PlayerDashboard />} />
+                    <Route path="/playermanagement" element={<PlayerManagement/>}/>
+                    <Route path="/schedulemanagement" element={<ScheduleManagement/>}/> 
+                    <Route path="/teammanagement" element={<TeamManagement/>}/>
+                    <Route path="/reportsmanagement" element={<ReportsManagement/>}/>
+                    <Route path="/playerstats" element={<PlayerStats/>}/>
+                    <Route path="/scheduleview" element={<ScheduleView/>}/>
+                    <Route path="/fitnesstracker" element={<FitnessTracker/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
